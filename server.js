f4cab69fd2d454bcf3631a2893d9c3dfb3ced274
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2025-09-16 14:59:42 +0530

    Repo initiation

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, world from express! The web server is working");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}/`);
+});
